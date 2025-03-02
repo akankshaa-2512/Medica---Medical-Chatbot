ad953367a67c674d025b22b1bdd0a6292b4a4d15
Akanksha <akankshaanand2512@gmail.com>
2025-03-02 21:11:35 +0530

    completed

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are an assistant for question answering task."
+    "Use the following retrieved data to answer the question."
+    "If you dont know the answer say it and apologise and ask how can you assist them in something else."
+    "Keep the answers concise."
+    "\n\n"
+    "{context}"
+)
